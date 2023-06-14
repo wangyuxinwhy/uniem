1532a463238a069f6c5fd2d4d920cf17d64578f3
wangyuxin <wangyuxin@mokahr.com>
2023-06-14 14:27:55 +0800

    add more model

--- mteb-zh/models.py
+++ mteb-zh/models.py
@@ -1,13 +1,15 @@
+from enum import Enum
 import logging
 import os
 import time
 from itertools import islice
-from typing import Generator, Iterable, Optional, TypeVar, cast
+from typing import Any, Generator, Iterable, Optional, Protocol, TypeVar, cast
 
 import torch
 import numpy as np
 import openai
 from tqdm import tqdm
+from sentence_transformers import SentenceTransformer
 
 os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
@@ -16,40 +18,51 @@ logging.basicConfig(level=logging.INFO)
 T = TypeVar('T')
 
 
+class MTEBModel(Protocol):
+    def encode(self, sentences: list[str], batch_size: int) -> Any:
+        ...
+
+
+class ModelType(str, Enum):
+    m3e_small = 'm3e_small'
+    m3e_base = 'm3e_base'
+    d_meta_soul = 'd_meta_soul'
+    uer = 'uer'
+    text2vec = 'text2vec'
+    openai = 'openai'
+    luotuo = 'luotuo'
+    erlangshen = 'erlangshen'
+    azure = 'azure'
+
+
+def load_model(model_type: ModelType) -> MTEBModel:
+    match model_type:
+        case ModelType.m3e_small:
+            return SentenceTransformer('moka-ai/m3e-small')
+        case ModelType.m3e_base:
+            return SentenceTransformer('moka-ai/m3e-base')
+        case ModelType.d_meta_soul:
+            return SentenceTransformer('DMetaSoul/sbert-chinese-general-v2')
+        case ModelType.uer:
+            return SentenceTransformer('uer/sbert-base-chinese-nli')
+        case ModelType.text2vec:
+            from text2vec import SentenceModel
+
+            return SentenceModel()
+        case ModelType.openai:
+            return OpenAIModel(model_name='text-embedding-ada-002')
+        case ModelType.luotuo:
+            return LuotuoBertModel(model_name='silk-road/luotuo-bert')
+        case ModelType.erlangshen:
+            return ErLangShenModel(model_name='IDEA-CCNL/Erlangshen-SimCSE-110M-Chinese')
+        case _:
+            raise ValueError(f'Unknown model type: {model_type}')
+
+
 def generate_batch(data: Iterable[T], batch_size: int = 32) -> Generator[list[T], None, None]:
     iterator = iter(data)
     while batch := list(islice(iterator, batch_size)):
         yield batch
-
-
-def load_model_by_name(name: str):
-    if 'm3e' in name:
-        from sentence_transformers import SentenceTransformer
-
-        model = SentenceTransformer(f'moka-ai/{name}')
-    elif 'text2vec' in name:
-        from text2vec import SentenceModel
-
-        model = SentenceModel()
-    elif 'openai' in name:
-        from models import OpenAIModel
-
-        model = OpenAIModel()
-    elif 'azure' in name:
-        from models import AzureModel
-
-        model = AzureModel()
-    elif 'luotuo' in name:
-        from models import LuotuoBertModel
-
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        if name == 'luotuo':
-            model = LuotuoBertModel(device=device)
-        else:
-            model = LuotuoBertModel(name, device=device)
-    else:
-        raise ValueError(f'Unknown model name: {name}')
-    return model
 
 
 class OpenAIModel:
@@ -59,9 +72,9 @@ class OpenAIModel:
         self._client = openai.Embedding
         self.model_name = model_name
 
-    def encode(self, texts: list[str], batch_size: int = 32, **kwargs) -> list[np.ndarray]:
+    def encode(self, sentences: list[str], batch_size: int = 32, **kwargs) -> list[np.ndarray]:
         all_embeddings = []
-        for batch in tqdm(generate_batch(texts, batch_size), total=len(texts) // batch_size):
+        for batch in tqdm(generate_batch(sentences, batch_size), total=len(sentences) // batch_size):
             embeddings = self._client.create(input=batch, engine=self.model_name)['data']   # type: ignore
             embeddings = sorted(embeddings, key=lambda e: e['index'])  # type: ignore
             embeddings = [np.array(result['embedding']) for result in embeddings]
@@ -78,9 +91,9 @@ class AzureModel:
         self._client = openai.Embedding
         self.model_name = model_name
 
-    def encode(self, texts: list[str], batch_size: int = 32, **kwargs) -> list[np.ndarray]:
+    def encode(self, sentences: list[str], batch_size: int = 32, **kwargs) -> list[np.ndarray]:
         all_embeddings = []
-        for text in tqdm(texts):
+        for text in tqdm(sentences):
             embeddings = self._client.create(input=text, engine=self.model_name)['data']   # type: ignore
             embeddings = [np.array(result['embedding']) for result in embeddings]
             all_embeddings.extend(embeddings)
@@ -88,19 +101,49 @@ class AzureModel:
         return all_embeddings
 
 
+class ErLangShenModel:
+    def __init__(self, model_name: str = 'IDEA-CCNL/Erlangshen-SimCSE-110M-Chinese', device: str | None = None) -> None:
+        from transformers import AutoTokenizer,AutoModelForMaskedLM
+        
+        if device is None:
+            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        else:
+            self.device = device
+        self.model =AutoModelForMaskedLM.from_pretrained(model_name)
+        self.model.to(self.device)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    def encode(self, sentences: list[str], batch_size: int = 32, **kwargs) -> list[np.ndarray]:
+        all_embeddings: list[np.ndarray] = []
+        for batch_texts in tqdm(generate_batch(sentences, batch_size), total=len(sentences) // batch_size):
+            inputs = self.tokenizer(batch_texts, padding=True, truncation=True, return_tensors="pt", max_length=512)
+            inputs = inputs.to(self.device)
+            with torch.no_grad():
+                outputs = self.model(**inputs ,output_hidden_states=True)
+                embeddings = outputs.hidden_states[-1][:,0,:].squeeze()
+            embeddings = cast(torch.Tensor, embeddings)
+            all_embeddings.extend(embeddings.cpu().numpy())
+        return all_embeddings
+
+
 class LuotuoBertModel:
-    def __init__(self, model_name: str = 'silk-road/luotuo-bert', device: str='cpu') -> None:
+    def __init__(self, model_name: str = 'silk-road/luotuo-bert', device: str | None = None) -> None:
         from transformers import AutoTokenizer, AutoModel
         from argparse import Namespace
+
+        if device is None:
+            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        else:
+            self.device = device
+
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         model_args = Namespace(do_mlm=None, pooler_type="cls", temp=0.05, mlp_only_train=False, init_embeddings_model=None)
         self.model = AutoModel.from_pretrained(model_name, trust_remote_code=True, model_args=model_args)
         self.model.to(device)
-        self.device = device
     
-    def encode(self, texts: list[str], batch_size: int = 32, **kwargs) -> list[np.ndarray]:
+    def encode(self, sentences: list[str], batch_size: int = 32, **kwargs) -> list[np.ndarray]:
         all_embeddings: list[np.ndarray] = []
-        for batch_texts in tqdm(self.generate_batch(texts, batch_size), total=len(texts) // batch_size):
+        for batch_texts in tqdm(generate_batch(sentences, batch_size), total=len(sentences) // batch_size):
             inputs = self.tokenizer(batch_texts, padding=True, truncation=True, return_tensors="pt", max_length=512)
             inputs = inputs.to(self.device)
             with torch.no_grad():
@@ -108,9 +151,3 @@ class LuotuoBertModel:
             embeddings = cast(torch.Tensor, embeddings)
             all_embeddings.extend(embeddings.cpu().numpy())
         return all_embeddings
-
-    @staticmethod
-    def generate_batch(data: Iterable[T], batch_size: int = 32) -> Generator[list[T], None, None]:
-        iterator = iter(data)
-        while batch := list(islice(iterator, batch_size)):
-            yield batch
--- mteb-zh/run_mteb_zh.py
+++ mteb-zh/run_mteb_zh.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
 import typer
-from models import load_model_by_name
+from models import load_model, ModelType
 from mteb import MTEB
 
 from tasks import GubaEastmony, IFlyTek, JDIphone, StockComSentiment, T2RRetrieval, TNews, TYQSentiment, T2RReranking
 
 
-def main(name: str, output_folder: Path = Path('results')):
+def main(name: ModelType, output_folder: Path = Path('results')):
     output_folder = Path(output_folder)
-    model = load_model_by_name(name)
+    model = load_model(name)
     evaluation = MTEB(
         tasks=[TYQSentiment(), TNews(), JDIphone(), StockComSentiment(), GubaEastmony(), IFlyTek(), T2RReranking(2), T2RRetrieval(100000)]
     )
