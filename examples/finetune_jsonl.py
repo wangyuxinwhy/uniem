61d039ea06a3f953c42f0a68d235d852ee528fcc
wangyuxin <wangyuxin@mokahr.com>
2023-07-26 13:23:08 +0800

    ✨feat:支持 M3E 分布式训练

--- examples/finetune_jsonl.py
+++ examples/finetune_jsonl.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from accelerate import DistributedDataParallelKwargs
 from uniem.finetuner import FineTuner
 
 # 读取 jsonl 文件
@@ -7,4 +8,11 @@ df = pd.read_json('example_data/riddle.jsonl', lines=True)
 df = df.rename(columns={'instruction': 'text', 'output': 'text_pos'})
 # 指定训练的模型为 m3e-small
 finetuner = FineTuner.from_pretrained('moka-ai/m3e-small', dataset=df.to_dict('records'))
-finetuner.run(epochs=1, output_dir='finetuned-model-riddle')
+finetuner.run(
+    epochs=1, 
+    output_dir='finetuned-model-riddle',
+    batch_size=32,
+    accelerator_kwargs={
+        'kwargs_handlers': [DistributedDataParallelKwargs(find_unused_parameters=True)]
+    }
+)
